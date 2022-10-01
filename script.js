2ff6238e2ac4e731e4a74b979fcb60eba5f36f10
Anton <33631140+savinbot@users.noreply.github.com>
2022-10-01 08:17:52 +0500

    Update script.js
    Signed-off-by: Anton <33631140+savinbot@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelector("button").addEventListener("click", function () {
   html2canvas(document.querySelector(".specific"), {
     onrendered: function (canvas) {
-      // document.body.appendChild(canvas);
+       document.body.appendChild(canvas);
       return Canvas2Image.saveAsPNG(canvas);
     }
   });
